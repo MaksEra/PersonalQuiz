d423a47860f5ab85c4900d20612a64f8e86ff812
Maks Era <maks9306@gmail.com>
2021-06-03 23:22:54 +0300

    Add question model

--- PersonalQuiz/Models/Question.swift
+++ PersonalQuiz/Models/Question.swift
@@ -5,4 +5,45 @@
 //  Created by Maks Era on 03.06.2021.
 //
 
+struct Question {
+    let title: String
+    let type: ResponseType
+    let answers: [Answer]
+    
+    static func getQuestion() -> [Question] {
+        [
+            Question(
+                title: "Стейк",
+                type: .single,
+                answers: [
+                    Answer(title: "", type: .cat),
+                    Answer(title: "", type: .dog)
+                ]),
+            Question(
+                title: "Вождение",
+                type: .ranged,
+                answers: [
+                    Answer(title: "", type: .cat),
+                    Answer(title: "", type: .dog)
+                ]),
+        ]
+    }
+}
 
+enum ResponseType {
+    case single
+    case multiple
+    case ranged
+}
+
+struct Answer {
+    let title: String
+    let type: AnimalType
+}
+
+enum AnimalType: Character {
+    case dog = "🐶"
+    case cat = "🐱"
+    case rabbit = "🐰"
+    case turtle = "🐢"
+}
--- PersonalQuiz/ViewControllers/ViewController.swift
+++ PersonalQuiz/ViewControllers/ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let questions = Question.getQuestion()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
